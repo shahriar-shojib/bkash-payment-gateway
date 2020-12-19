66efda23406d01c117e2266e5d14e5d8e1d97e32
Shahriar Shojib <shahriar_shojib@hotmail.com>
2020-12-19 20:09:35 +0600

    updated example code

--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { bkashController } = require('./bkashController');
 
 const app = express();
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3000;
 app.use('/bkash', bkashController);
 app.listen(port, () => console.log('Server started @' + port));
--- examples/modules/bkash.js
+++ examples/modules/bkash.js
@@ -1,11 +1,11 @@
 const BkashGateway = require('bkash-payment-gateway');
 
 const bkash = new BkashGateway({
-	baseURL: 'https://checkout.sandbox.bka.sh/v1.2.0-beta/',
-	key: 'abcdxx2369',
-	username: 'bkashTest',
-	password: 'bkashPassword1',
-	secret: 'bkashSup3rS3cRet',
+	baseURL: 'https://checkout.sandbox.bka.sh/v1.2.0-beta',
+	key: '5tunt4masn6pv2hnvte1sb5n3j',
+	username: 'sandboxTestUser',
+	password: 'hWD@8vtzw0',
+	secret: '1vggbqd4hqk9g96o9rrrp2jftvek578v7d2bnerim12a87dbrrka',
 });
 
 module.exports = bkash;
